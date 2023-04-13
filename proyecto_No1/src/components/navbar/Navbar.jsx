cecbd5067fd307ca253d8f00c8247f5fd5db645f
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-12 22:19:30 -0600

    Add navbar

---
+++ proyecto_No1/src/components/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import './Navbar.css'
+
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="logo">
+            <img src={logo} alt="logo" />
+            <div className="discord">Discord</div>
+        </div>
+
+        <div className="menu">
+            <ul className='menu-link'>
+                <li>Download</li>
+                <li>Nitro</li>
+                <li>Discover</li>
+                <li>Safety</li>
+                <li>Support</li>
+                <li>Blog</li>
+                <li>Careers</li>
+            </ul>
+        </div>
+
+        <div className="openDiscord">
+            <button className='openDiscorButton'>Open Discord</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
