d5f6a1620ee07309cf0c3e53c77c30b0f6bf2cfc
Marco Ramirez <mvrcentes@hotmail.com>
2023-05-20 17:31:50 -0600

    Add Storybook

---
+++ src/components/navbar/Navbar.stories.js
@@ -0,0 +1,16 @@
+import  Navbar  from './Navbar';
+
+export default {
+  title: 'Example/Navbar',
+  component: Navbar,
+  tags: ['autodocs'],
+  argTypes: {
+  },
+};
+
+
+export const Primary = {
+  args: {
+    label: 'Navbar',
+  },
+};
